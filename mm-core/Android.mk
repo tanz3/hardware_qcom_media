38e0b48d64213a291c64f88ea33ae8f9495a7a44
Surajit Podder <spodder@codeaurora.org>
2018-05-18 16:20:52 +0530

    mm-core: add MSMNILE(sm8150) to supported targets
    Keep single file for registry for all targets,
    and use macro for listing registry entries.
    Remove unused targets from make file.
    
    Change-Id: I68d029ace103a658628ce3324ab0a6087ab84ad7

--- mm-core/Android.mk
+++ mm-core/Android.mk
@@ -23,6 +23,8 @@ endif
 
 ifeq ($(filter $(TARGET_BOARD_PLATFORM), bengal),$(TARGET_BOARD_PLATFORM))
 OMXCORE_CFLAGS += -D_BENGAL_
+else ifeq ($(filter $(TARGET_BOARD_PLATFORM), $(MSMNILE)),$(TARGET_BOARD_PLATFORM))
+OMXCORE_CFLAGS += -D_NILE_
 else ifeq ($(filter $(TARGET_BOARD_PLATFORM), $(MSMSTEPPE)),$(TARGET_BOARD_PLATFORM))
 OMXCORE_CFLAGS += -D_STEPPE_
 else ifeq ($(filter $(TARGET_BOARD_PLATFORM), sdm845),$(TARGET_BOARD_PLATFORM))
@@ -70,7 +72,7 @@ LOCAL_CFLAGS            := $(OMXCORE_CFLAGS)
 
 LOCAL_SRC_FILES         := src/common/omx_core_cmp.cpp
 LOCAL_SRC_FILES         += src/common/qc_omx_core.c
-ifneq (,$(filter lito bengal kona sdm845 $(MSMSTEPPE),$(TARGET_BOARD_PLATFORM)))
+ifneq (,$(filter lito bengal kona sdm845 $(MSMNILE) $(MSMSTEPPE),$(TARGET_BOARD_PLATFORM)))
 LOCAL_SRC_FILES         += src/registry_table_android.c
 else
 LOCAL_SRC_FILES         += src/default/qc_registry_table_android.c
@@ -115,7 +117,7 @@ endif
 
 LOCAL_SRC_FILES         := src/common/omx_core_cmp.cpp
 LOCAL_SRC_FILES         += src/common/qc_omx_core.c
-ifneq (,$(filter lito bengal kona sdm845 $(MSMSTEPPE),$(TARGET_BOARD_PLATFORM)))
+ifneq (,$(filter lito bengal kona sdm845 $(MSMNILE) $(MSMSTEPPE),$(TARGET_BOARD_PLATFORM)))
 LOCAL_SRC_FILES         += src/$(MM_CORE_TARGET)/registry_table.c
 else
 LOCAL_SRC_FILES         += src/$(MM_CORE_TARGET)/default/qc_registry_table.c
